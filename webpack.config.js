20c873e7d54c20a1fe7627cfe477eda0d805d7a1
petrbrzek <petrbrzek@abdoc.net>
2014-10-07 17:45:07 +0200

    Added live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-  entry: "./index.js",
-  output: {
-      path: __dirname,
-      filename: "bundle.js"
-  },
-	module: {
-		loaders: [
-			{ test: /\.coffee$/, loader: "coffee" },
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" }
-		]
-	},
-	resolve: {
-		extensions: ["", ".coffee", ".js"]
-	}
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/dev-server',
+        './index.js'
+    ],
+    devtool: 'source-map',
+    debug: true,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.coffee$/,
+            loader: 'coffee'
+        }, {
+            test: /\.less$/,
+            loaders: ['style', 'css', 'less']
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'url-loader?limit=8192'
+        }],
+        noParse: /\.min\.js/
+    },
+    resolve: {
+        extensions: ['', '.coffee', '.js']
+    },
+    resolveLoader: {
+        modulesDirectories: ['node_modules']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
